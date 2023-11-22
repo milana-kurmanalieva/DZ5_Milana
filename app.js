e19393edaf92542ba9eab5c7c7c6ebde9e930e61
milana-kurmanalieva <milanakurmanalieva@23gmail.com>
2023-11-22 12:45:06 +0600

    dz5

---
+++ app.js
@@ -0,0 +1,37 @@
+const numbers = [2, 50, 8, 10, 4, 20, 3, 7, 0, 6, 90, 2, 8, 10, 50, 3, 7, 4, 6, 0, 90]
+const resultNumbers = Array.from(new Set(numbers))
+
+console.log(resultNumbers)
+
+
+
+function books (bookMap) {
+    const bookForMap = bookMap.filter(book => book.includes('у') || book.includes('У'))
+    const bookForFilter = bookMap.filter(book => !book.includes('У') || !book.includes('у'))
+    const booksResult = bookForMap.map(book => ' Книга с буквой у: ' + book )
+    console.log(`книги с буквой у`)
+    booksResult.forEach(book => console.log(book))
+    console.log('Остальные книги: ')
+    bookForFilter.forEach(book => console.log(book))
+}
+
+const forBook = ['У лука море дуб зеленый', 'Война и мир','Улыбайся', 'Любовь и предубеждения', 'У страха глаза велики', 'Оно' ,'hello world','Сумерки' ,'Женская логика']
+books(forBook)
+
+const users = [
+    {name:'John', age: 45},
+    {name:'BOB', age: 14},
+    {name:'Aziz', age: 15},
+    {name:'Malika', age: 76},
+    {name:'Mariya', age: 23},
+    {name:'Aidana', age: 21},
+    {name:'Aalam', age: 23},
+    {name:'Leo', age: 17},
+]
+function usersAges (users){
+    const youngUser = users.reduce((min, user) => (user.age < min.age ? user : min))
+    const oldUser = users.reduce((max, user) => (user.age > max.age ? user : max))
+    const youngAge = Math.abs(oldUser.age - youngUser.age)
+    console.log(`Пользователь ${oldUser.name} старше ${youngUser.name} на ${youngAge} лет`)
+}
+usersAges(users)
